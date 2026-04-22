4a3b18e857cce4b94b5e6c8ec39cec77fc809bca
smsohelrana420 <mirzasohel550@gmail.com>
2026-04-22 09:39:19 +0600

    navbar create done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="bg-[#2d2b33] shadow-xl">
+      <div className="container">
+        <div className="sm:flex justify-between py-6 items-center">
+          <div className='text-center mb-3 sm:mb-0'>
+            <a
+              href="#"
+              className="text-white text-xl sm:text-3xl font-bold font-poppins"
+            >
+              <span className="text-[#F65023]">S</span>mart Cod
+              <span className="text-[#F65023]">ers</span>
+            </a>
+          </div>
+          <ul className="sm:flex text-white font-poppins text-xl gap-8 text-center border-t sm:border-none">
+            <li className='mt-4 sm:mt-0'>
+              <a href="#" className="text-[#F65023]">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-[#F65023]">
+                Products
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-[#F65023]">
+                Blog
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-[#F65023]">
+                Portfolio
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-[#F65023]">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar
